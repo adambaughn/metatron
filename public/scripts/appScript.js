42a0e5ec580e7fcdacf8cddaba31a63a1b3f6dfc
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-03 13:58:54 -0400

    Add files via upload

---
+++ public/scripts/appScript.js
@@ -0,0 +1,21 @@
+﻿const path = require("path");
+const events = require("events");
+const readline = require("readline");
+const fs = require("fs");
+const cp = require("child_process");
+
+function createFile (textInput, nameInput) {
+    var textAreaInput = textInput;
+    var fileNameInput = nameInput;
+    var fileSavedText = document.getElementById("fileSaved");
+
+    fs.writeFile(`./assets/${fileNameInput}.md`, textAreaInput.trim(), err => {
+        if (err) {
+            throw err;
+        }
+        fileSavedText = nameInput;
+    });
+
+}
+
+
