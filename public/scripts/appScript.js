d6b1bffb65ec350d22818ef490a448b5935b60e7
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-03 15:20:01 -0400

    Add files via upload

--- public/scripts/appScript.js
+++ public/scripts/appScript.js
@@ -3,10 +3,10 @@ const events = require("events");
 const readline = require("readline");
 const cp = require("child_process");
 
-function createFile (textInput, nameInput) {
+function createFile () {
     const fs = require("fs");
-    var textAreaInput = textInput;
-    var fileNameInput = nameInput;
+    var textAreaInput = document.getElementById('essentialTraining_textarea').value;
+    var fileNameInput = document.getElementById('nameInput').value;
 
     fs.writeFile(`./assets/${fileNameInput}.md`, textAreaInput.trim(), err => {
         if (err) {
